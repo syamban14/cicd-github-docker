0dc9496b0fbf94522d2c07d85e142ecd0a52ad8b
M. Syamsul <58071759+syamban14@users.noreply.github.com>
2022-07-22 10:56:24 +0700

    Menambah teks baru

--- main.go
+++ main.go
@@ -7,4 +7,5 @@ func main() {
 	fmt.Println("Belajar Go dengan Vim")
 	fmt.Println("Akan di Integrasi degan Github dan di Deploy ke Docker")
 	fmt.Println("Upload dengan Push dan Download dengan Pull")
+	fmt.Println("Terimakasih")
 }
