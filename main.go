4cb9399e9c15c643a803649d84aab31c314e9d40
syamban14 <syamsulikj.48@gmail.com>
2022-07-22 10:55:12 +0700

    Menambahkan teks baru lagi

--- main.go
+++ main.go
@@ -6,4 +6,5 @@ func main() {
 	fmt.Println("vim-go")
 	fmt.Println("Belajar Go dengan Vim")
 	fmt.Println("Akan di Integrasi degan Github dan di Deploy ke Docker")
+	fmt.Println("Upload dengan Push dan Download dengan Pull")
 }
